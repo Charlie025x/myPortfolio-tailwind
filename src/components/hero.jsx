2fbadcdeb335180bda4ee0c2af54ae3bcacd163b
Charlie025x <charliealonso2001@gmail.com>
2023-11-07 16:27:41 -0600

    added typewriter component for hero type effect

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,3 +1,5 @@
+import Typewriter from "typewriter-effect";
+
 export default function Hero() {
   return (
     <section
@@ -6,13 +8,18 @@ export default function Hero() {
       className="flex min-h-screen w-full items-center justify-center bg-slate-500 bg-hero-img bg-fixed"
     >
       <div className="hero-container " data-aos="fade-in">
-        <h1 className="text-3xl font-bold text-white ">Charlie Alonso</h1>
-        <p className="text-2xl font-bold text-white ">
+        <h1 className=" text-6xl font-bold text-white">Charlie Alonso</h1>
+        <p className="text-2xl font-bold text-white">
           I'm a{" "}
-          <span
-            className="typed"
-            data-typed-items="Designer, Developer, Researcher, Problem Solver, Engineer, Self Starter, Artist"
-          ></span>
+          <span>
+            <Typewriter //added Dispay:inline in index.css
+              options={{
+                strings: ["Developer", "Athlete", "Artist", "Brother", "Son"],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </span>
         </p>
       </div>
     </section>
